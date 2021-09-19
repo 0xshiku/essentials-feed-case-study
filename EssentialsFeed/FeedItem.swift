33994c8ec3077601e99f8ed6ae5c737e3a0732d8
Chico Pereira <olachico@icloud.com>
2021-09-19 20:26:28 +0100

    Create the FeedItem Model

---
+++ EssentialsFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialsFeed
+//
+//  Created by Chico Pereira on 19/09/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
