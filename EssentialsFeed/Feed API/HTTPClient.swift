7a9662cba10153850e8e2f22959d2120ec4ea29b
Chico Pereira <olachico@icloud.com>
2021-10-01 00:16:25 +0100

    Move RemoteFeedLoader components to Production

---
+++ EssentialsFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,12 @@
+//
+//  HTTPClient.swift
+//  EssentialsFeed
+//
+//  Created by Chico Pereira on 01/10/2021.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
---
+++ EssentialsFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,22 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialsFeed
+//
+//  Created by Chico Pereira on 01/10/2021.
+//
+
+import Foundation
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
