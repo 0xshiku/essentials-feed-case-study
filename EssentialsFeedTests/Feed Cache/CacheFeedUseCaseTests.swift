a6cca75f53614e4af8121f47957603072a520493
Chico Pereira <olachico@icloud.com>
2021-11-06 15:15:12 +0000

    Add LocalFeedItem data transfer representation to decouple storage frameworks from FeedItem data models

--- EssentialsFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialsFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -39,10 +39,11 @@ class CacheFeedUseCaseTests: XCTestCase {
         let timestamp = Date()
         let (store, sut) = makeSUT(currentDate:  { timestamp })
         let items = [uniqueItem(), uniqueItem()]
+        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL)}
         sut.save(items) { _ in }
         store.completeDeletionSuccessfully()
         
-        XCTAssertEqual(store.receivedMessages, [.deletedCachedFeed, .insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessages, [.deletedCachedFeed, .insert(localItems, timestamp)])
     }
     
     func test_save_failsOnDeletionError() {
@@ -139,11 +140,11 @@ class CacheFeedUseCaseTests: XCTestCase {
     
     private class FeedStoreSpy: FeedStore {
         var deleteCacheFeedCallCount = 0
-        var insertions = [(items: [FeedItem], timestamp: Date)]()
+        var insertions = [(items: [LocalFeedItem], timestamp: Date)]()
         
         enum ReceivedMessage: Equatable {
             case deletedCachedFeed
-            case insert([FeedItem], Date)
+            case insert([LocalFeedItem], Date)
         }
         
         private(set) var receivedMessages = [ReceivedMessage]()
@@ -165,7 +166,7 @@ class CacheFeedUseCaseTests: XCTestCase {
             deletionCompletions[index](nil)
         }
         
-        func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
             insertionCompletions.append(completion)
             receivedMessages.append(.insert(items, timestamp))
         }
