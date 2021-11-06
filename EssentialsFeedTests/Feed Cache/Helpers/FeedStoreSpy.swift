8861c0983627b1239055d0b875f6a0371da76789
Chico Pereira <olachico@icloud.com>
2021-11-06 20:26:46 +0000

    Load command requests cache retrieval

---
+++ EssentialsFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,57 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialsFeedTests
+//
+//  Created by Chico Pereira on 06/11/2021.
+//
+
+import Foundation
+import EssentialsFeed
+
+class FeedStoreSpy: FeedStore {
+    var deleteCacheFeedCallCount = 0
+    var insertions = [(items: [LocalFeedImage], timestamp: Date)]()
+    
+    enum ReceivedMessage: Equatable {
+        case deletedCachedFeed
+        case insert([LocalFeedImage], Date)
+        case retrieve
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deleteCacheFeedCallCount += 1
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deletedCachedFeed)
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
+    }
+    
+}
