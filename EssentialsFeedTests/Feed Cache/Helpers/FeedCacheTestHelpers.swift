64c63a6e6111ad56e68296f1ef0af2dd03f31ea2
Chico Pereira <olachico@icloud.com>
2021-11-13 12:30:24 +0000

    Extract duplicated test helpers into a shared scope

---
+++ EssentialsFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -0,0 +1,29 @@
+//
+//  FeedCacheTestHelpers.swift
+//  EssentialsFeedTests
+//
+//  Created by Chico Pereira on 13/11/2021.
+//
+
+import Foundation
+import EssentialsFeed
+
+func uniqueImage() -> FeedImage {
+    return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+}
+
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage()]
+    let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)}
+    return (models, local)
+}
+
+extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
+}
---
+++ EssentialsFeedTests/Helpers/SharedHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  SharedHelperTests.swift
+//  EssentialsFeedTests
+//
+//  Created by Chico Pereira on 13/11/2021.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
