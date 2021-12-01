6edd9a51bc17a68d67c10889ba09a57685c54bf1
Chico Pereira <olachico@icloud.com>
2021-12-01 18:23:30 +0000

    Implements `CoreDataFeedStore` production code and test code

--- EssentialsFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialsFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -8,98 +8,88 @@
 import XCTest
 import EssentialsFeed
 
-class CoreDataFeedStoreTests: XCTestCase, FailableFeedStore {
-    
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-    
-    func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = makeSUT()
-        
-        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = makeSUT()
-        
-        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
-    }
-    
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-        
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-        
-    }
-    
-    func test_retrieve_deliversFailureOnRetrievalError() {
-        
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnFailure() {
-        
-    }
-    
-    func test_insert_deliversNoErrorOnEmptyCache() {
-        
-    }
-    
-    func test_insert_deliversNoErrorOnNonEmptyCache() {
-        
-    }
-    
-    func test_insert_overridesPreviouslyInsertedCacheValues() {
-        
-    }
-    
-    func test_insert_deliversErrorOnInsertionError() {
-        
-    }
-    
-    func test_insert_hasNoSideEffectsOnInsertionError() {
-        
-    }
-    
-    func test_delete_deliversNoErrorOnEmptyCache() {
-        
-    }
-    
-    func test_delete_hasNoSideEffectsOnEmptyCache() {
-        
-    }
-    
-    func test_delete_deliversNoErrorOnNonEmptyCache() {
-        
-    }
-    
-    func test_delete_emptiesPreviouslyInsertedCache() {
-        
-    }
-    
-    func test_delete_deliversErrorOnDeletionError() {
-        
-    }
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 
-    func test_delete_hasNoSideEffectsOnDeletionError() {
-        
-    }
-    
-    func test_storeSideEffects_runSerially() {
-        
-    }
-    
-    // - MARK: Helpers
-    
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-        let sut = CoreDataFeedStore()
-        trackForMemoryLeaks(sut, file: file, line: line)
-        return sut
-    }
-    
-}
+     func test_retrieve_deliversEmptyOnEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+     }
+
+     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+     }
+
+     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+     }
+
+     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+     }
+
+     func test_insert_deliversNoErrorOnEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+     }
+
+     func test_insert_deliversNoErrorOnNonEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+     }
+
+     func test_insert_overridesPreviouslyInsertedCacheValues() {
+         let sut = makeSUT()
+
+         assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
+     }
+
+     func test_delete_deliversNoErrorOnEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+     }
+
+     func test_delete_hasNoSideEffectsOnEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+     }
+
+     func test_delete_deliversNoErrorOnNonEmptyCache() {
+         let sut = makeSUT()
+
+         assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+     }
+
+     func test_delete_emptiesPreviouslyInsertedCache() {
+         let sut = makeSUT()
+
+         assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+     }
+
+     func test_storeSideEffects_runSerially() {
+         let sut = makeSUT()
+
+         assertThatSideEffectsRunSerially(on: sut)
+     }
+
+     // - MARK: Helpers
+     
+     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+         let storeBundle = Bundle(for: CoreDataFeedStore.self)
+         let storeURL = URL(fileURLWithPath: "/dev/null")
+         let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+         trackForMemoryLeaks(sut, file: file, line: line)
+         return sut
+     }
+
+ }
