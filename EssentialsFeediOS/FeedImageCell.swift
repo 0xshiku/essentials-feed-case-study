40ab0581cca491b65c93f640604f448dfefb798c
Chico Pereira <olachico@icloud.com>
2022-01-01 16:03:16 +0000

    Render loaded feed on successful load completion

---
+++ EssentialsFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialsFeediOS
+//
+//  Created by Chico Pereira on 31/12/2021.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
