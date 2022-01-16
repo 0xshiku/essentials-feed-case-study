ae30cb797bba3705a2371585e64c0d5de5af72f3
Chico Pereira <olachico@icloud.com>
2022-01-16 22:25:07 +0000

    Move main thread dispatch to the Presenter

--- EssentialsFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialsFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -14,15 +14,7 @@ protocol FeedViewControllerDelegate {
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
     var delegate: FeedViewControllerDelegate?
     var tableModel = [FeedImageCellController]() {
-        didSet {
-            if Thread.isMainThread {
-                tableView.reloadData()
-            } else {
-                DispatchQueue.main.async { [weak self] in
-                    self?.tableView.reloadData()
-                }
-            }
-        }
+        didSet { tableView.reloadData() }
     }
     
     public override func viewDidLoad() {
@@ -37,11 +29,6 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { [weak self] in
-                self?.display(viewModel)
-            }
-        }
         if viewModel.isLoading {
             refreshControl?.beginRefreshing()
         } else {
