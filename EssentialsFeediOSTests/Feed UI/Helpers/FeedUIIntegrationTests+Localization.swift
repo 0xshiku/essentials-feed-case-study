460a1c99f76b4141134df150cce5018f37af4ed8
Chico Pereira <olachico@icloud.com>
2022-01-16 19:16:54 +0000

    Add localisation tests to guarantee all localised keys have translations in all supported localizations

---
+++ EssentialsFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -0,0 +1,24 @@
+//
+//  FeedUIIntegrationTests.swift
+//  EssentialsFeediOSTests
+//
+//  Created by Chico Pereira on 16/01/2022.
+//
+
+import Foundation
+import XCTest
+import EssentialsFeediOS
+
+extension FeedUIIntegrationTests {
+    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
+
+
