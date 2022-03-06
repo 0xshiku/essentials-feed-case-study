afba838d9279d360bdd897dccbabb3edcf0ce752
Chico Pereira <olachico@icloud.com>
2022-03-06 19:25:33 +0000

    Move `FeedLoaderCacheDecorator` to production

---
+++ EssentialsApp/EssentialsApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,28 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  EssentialsApp
+//
+//  Created by Chico Pereira on 06/03/2022.
+//
+
+import Foundation
+import EssentialsFeed
+
+public final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map {feed in
+                self?.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
