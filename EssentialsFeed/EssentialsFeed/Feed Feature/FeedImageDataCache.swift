ac5e52d158006b06375508eea09e37b14a463be3
Chico Pereira <olachico@icloud.com>
2022-03-12 15:25:06 +0000

    Move `FeedImageDataCache` to the `Feed Feature` production group

---
+++ EssentialsFeed/EssentialsFeed/Feed Feature/FeedImageDataCache.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageDataCache.swift
+//  EssentialsFeed
+//
+//  Created by Chico Pereira on 12/03/2022.
+//
+
+import Foundation
+
+public protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
