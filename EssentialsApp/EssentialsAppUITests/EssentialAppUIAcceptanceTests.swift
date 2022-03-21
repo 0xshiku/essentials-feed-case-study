cd8ddb0ec06e51be220f7b477350d579f7ca4cb4
Chico Pereira <olachico@icloud.com>
2022-03-21 00:40:21 +0000

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness - We can now UI test without internet connection

--- EssentialsApp/EssentialsAppUITests/EssentialAppUIAcceptanceTests.swift
+++ EssentialsApp/EssentialsAppUITests/EssentialAppUIAcceptanceTests.swift
@@ -15,7 +15,7 @@ class EssentialAppUIAccentanceTests: XCTestCase {
         app.launch()
         
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
