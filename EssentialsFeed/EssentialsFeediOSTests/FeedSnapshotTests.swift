eaa1cc563ce88ab80759613b3abef8931e5ab49e
Chico Pereira <olachico@icloud.com>
2022-04-11 10:22:42 +0100

    Record feed with failed image loading snapshot

--- EssentialsFeed/EssentialsFeediOSTests/FeedSnapshotTests.swift
+++ EssentialsFeed/EssentialsFeediOSTests/FeedSnapshotTests.swift
@@ -34,6 +34,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT() -> FeedViewController {
@@ -52,6 +60,13 @@ class FeedSnapshotTests: XCTestCase {
         return [
             ImageStub(description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on 1,316m long remnant of the Berlin Wall, located near the centre of Berlin, on Muhlenstrabe in Friedrichshain-Kreuzberg. The gallery has official status as Denkmal, or heritage-protected landmark.", location: "East Side Gallery\nMemorial in Berlin, Germany", image: UIImage.make(withColor: .red)),
             ImageStub(description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.", location: "Garth Pier", image: UIImage.make(withColor: .green))
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(description: nil, location: "Cannon Street, London", image: nil),
+            ImageStub(description: nil, location: "Brighton Seafront", image: nil)
         ]
     }
     
