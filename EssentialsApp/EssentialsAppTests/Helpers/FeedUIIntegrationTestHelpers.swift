f763e3c66149d571a7580740791d86d5519983bc
Chico Pereira <olachico@icloud.com>
2022-06-12 21:52:50 +0100

    Render comments

--- EssentialsApp/EssentialsAppTests/Helpers/FeedUIIntegrationTestHelpers.swift
+++ EssentialsApp/EssentialsAppTests/Helpers/FeedUIIntegrationTestHelpers.swift
@@ -142,6 +142,41 @@ extension ListViewController {
     var isShowingLoadingIndicator: Bool {
         return refreshControl?.isRefreshing == true
     }
+}
+
+extension ListViewController {
+    func numberOfRenderedComments() -> Int {
+        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
+    }
+    
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+    
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+    
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel.text
+    }
+    
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else {
+            return nil
+        }
+        
+        let dataSource = tableView.dataSource
+        let index = IndexPath(row: row, section: commentsSection)
+        return dataSource?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+    
+    var commentsSection: Int {
+        return 0
+    }
+}
+
+extension ListViewController {
 
     func numberOfRenderedFeedImageViews() -> Int {
         tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: feedImagesSection)
