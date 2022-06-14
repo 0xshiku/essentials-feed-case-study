fd8a2005dd05f036d47e5e17c5707b67d0af0e1a
Chico Pereira <olachico@icloud.com>
2022-06-15 00:03:10 +0100

    Add "Load More" cell controller with loading indicator

---
+++ EssentialsFeed/EssentialsFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadMoreCellController.swift
+//  EssentialsFeediOS
+//
+//  Created by Chico Pereira on 14/06/2022.
+//
+
+import UIKit
+import EssentialsFeed
+import EssentialsFeedPresentation
+
+public class LoadMoreCellController: NSObject, UITableViewDataSource {
+    
+    private let cell = LoadMoreCell()
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell
+    }
+    
+}
+
+extension LoadMoreCellController: ResourceLoadingView {
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell.isLoading = viewModel.isLoading
+    }
+}
