633f9d64a9486748054edfeb865a415564e95cc2
Chico Pereira <olachico@icloud.com>
2022-06-17 22:32:55 +0100

    Set FeedEndpoint limit to 10

---
+++ EssentialsFeed/EssentialsFeedAPITests/FeedEndpointTests.swift
@@ -0,0 +1,24 @@
+//
+//  FeedEndpointTests.swift
+//  EssentialsFeedAPITests
+//
+//  Created by Chico Pereira on 17/06/2022.
+//
+
+import XCTest
+import EssentialsFeedAPI
+
+class FeedEndpointTests: XCTestCase {
+    
+    func test_feed_endpointURL() {
+        let baseURL = URL(string: "http://base-url.com")!
+        
+        let received = FeedEndpoint.get.url(baseURL: baseURL)
+        
+        XCTAssertEqual(received.scheme, "http", "scheme")
+        XCTAssertEqual(received.host, "base-url.com", "host")
+        XCTAssertEqual(received.path, "/v1/feed", "path")
+        XCTAssertEqual(received.query, "limit=10", "query")
+    }
+    
+}
