56208c27c9c015dc6eba574ddd53ab3a6884621f
Chico Pereira <olachico@icloud.com>
2022-06-19 20:15:11 +0100

    Use `NullStore` when we can't create a `CoreDataFeedStore` instance

---
+++ EssentialsApp/EssentialsApp/NullStore.swift
@@ -0,0 +1,31 @@
+//
+//  NullStore.swift
+//  EssentialsApp
+//
+//  Created by Chico Pereira on 19/06/2022.
+//
+
+import Foundation
+import EssentialsFeedCache
+
+class NullStore: FeedStore & FeedImageDataStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        completion(.success(()))
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.none))
+    }
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+        completion(.success(()))
+    }
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
